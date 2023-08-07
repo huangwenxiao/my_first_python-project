a8077cf5fbc2413f3d308aa525a2c32b25749a68
huangwenxiao <107984336+huangwenxiao@users.noreply.github.com>
2023-08-07 23:02:51 +0800

    Delete tests directory

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-def get_formatted_name(first_name,last_name):
-    """return name"""
-    full_name=f"{first_name}{last_name}"
-
-    return full_name.title()
-musician=get_formatted_name("jimi","hendrix")
-print(musician)
